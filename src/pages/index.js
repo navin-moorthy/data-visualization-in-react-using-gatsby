7c1a6d2e24d5e4007b127b71c749f57e494b2d60
NavinNavi19 <navin007.a@gmail.com>
2019-08-26 17:43:01 +0000

    Initial Render with random 20 data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import * as D3 from "d3"
 
-export default () => <div>Hello world!</div>
+const getRandomData = () =>
+  D3.range(20).map(() => ({ x: Math.random(), y: Math.random() }))
+
+export default () => {
+  const data = getRandomData()
+  const xScale = D3.scaleLinear()
+    .domain([0, 1])
+    .range([0, 400])
+  const yScale = D3.scaleLinear()
+    .domain([0, 1])
+    .range([0, 400])
+
+  return (
+    <svg width={400} height={400}>
+      {data.map(d => (
+        <circle cx={xScale(d.x)} cy={yScale(d.y)} id={d.x} r={10} />
+      ))}
+    </svg>
+  )
+}
